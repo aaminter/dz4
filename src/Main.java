84d4a80c12ba58384e30a3f253088908caafa9bd
aaminter <aam.interpolyaris@gmail.com>
2025-03-16 17:49:59 +0300

    Added 8 exercises

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,66 @@
 public class Main {
     public static void main(String[] args) {
-        var dog = 8.0; //8
-        var cat = 3.6; //3.6
-        var paper = 763789; //763789
-        System.out.println(); //Задача 1
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println();
         System.out.println("Задача 1:");
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
-        System.out.println(); //Задача 2
+        System.out.println();
         System.out.println("Задача 2:");
-        System.out.println(dog + 4); //12
-        System.out.println(cat + 4); //7.6
-        System.out.println(paper + 4); //763793
-        System.out.println(); //Задача 3
+        var dogFour = dog + 4;
+        var catFour = dog + 4;
+        var paperFour = dog + 4;
+        System.out.println(dogFour);
+        System.out.println(catFour);
+        System.out.println(paperFour);
+        System.out.println();
         System.out.println("Задача 3:");
-        System.out.println(dog - 3.5); //4.5
-        System.out.println(cat - 1.6); //2.0
-        System.out.println(paper - 7639); //756150
-        var friend = 19; // 19
-        System.out.println(); //Задача 4
+        var dogMinus = dog - 3.5f;
+        var catMinus = dog - 1.6f;
+        var paperMinus = dog - 7639;
+        System.out.println(dogMinus);
+        System.out.println(catMinus);
+        System.out.println(paperMinus);
+        var friend = 19;
+        var friendPlusTwo = friend + 2;
+        var friendDivSeven = friendPlusTwo / 7;
+        System.out.println();
         System.out.println("Задача 4:");
-        System.out.println(friend); //19
-        System.out.println(friend + 2); //21
-        System.out.println((friend + 2) / 7); //3
-        var frog = 3.5; //3.5
-        System.out.println(); //Задача 5
+        System.out.println(friend);
+        System.out.println(friendPlusTwo);
+        System.out.println(friendDivSeven);
+        var frog = 3.5f;
+        var frogMultiTen = frog * 10;
+        var frogDivThree = frogMultiTen / 3.5f;
+        var frogPlusFour = frogDivThree + 4;
+        System.out.println();
         System.out.println("Задача 5:");
-        System.out.println(frog * 10); //35.0
-        System.out.println((frog * 10) / 3.5); //10.0
-        System.out.println((frog * 10) / 3.5 + 4); //14.0
-        var boxerWeight1 = 78.2; //78.2
-        var boxerWeight2 = 82.7; //82.7
-        System.out.println(); //Задача 6
+        System.out.println(frogMultiTen);
+        System.out.println(frogDivThree);
+        System.out.println(frogPlusFour);
+        var boxerWeightFirst = 78.2f;
+        var boxerWeightSecond = 82.7f;
+        var allMass = boxerWeightFirst + boxerWeightSecond;
+        var diffMass = boxerWeightSecond - boxerWeightFirst;
+        System.out.println();
         System.out.println("Задача 6:");
-        System.out.println("Общая масса: " + (boxerWeight1 + boxerWeight2) + " кг"); //160.9
-        System.out.println("Разница масс: " + (boxerWeight2 - boxerWeight1) + " кг"); //4.5
-        System.out.println(); //Задача 7
+        System.out.println("Общая масса: " + allMass + " кг");
+        System.out.println("Разница масс: " + diffMass + " кг");
+        System.out.println();
         System.out.println("Задача 7:");
-        System.out.println("Остаток от деления масс: " + (boxerWeight2 % boxerWeight1) + " кг"); //4.5
-        var jobTime = 640; //640
-        var workerTime = 8; //8
-        System.out.println(); //Задача 8
+        var remDivMass = boxerWeightSecond % boxerWeightFirst;
+        System.out.println("Остаток от деления масс: " + remDivMass + " кг");
+        var jobTime = 640;
+        var workerTime = 8;
+        var allEmployee = jobTime / workerTime;
+        var allEmployeePlusNintyFour = jobTime / workerTime + 94;
+        var allTimeForEmployee = jobTime / allEmployeePlusNintyFour;
+        System.out.println();
         System.out.println("Задача 8:");
-        System.out.println("Всего сотрудников в компании - " + (jobTime / workerTime) + " человек"); //80
-        System.out.println("Если в компании работает - " + (jobTime / workerTime + 94) + " человека," + " то всего " + (jobTime / (jobTime / workerTime + 94)) + " часа работы может быть поделено между сотрудниками"); //174 и 3
+        System.out.println("Всего сотрудников в компании - " + allEmployee + " человек");
+        System.out.println("Если в компании работает - " + allEmployeePlusNintyFour + " человека," + " то всего " + allTimeForEmployee + " часа работы может быть поделено между сотрудниками");
         }
 }
